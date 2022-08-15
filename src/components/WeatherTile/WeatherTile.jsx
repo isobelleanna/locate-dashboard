02bc28d9fe2140afdcf5e7518c843bb038f15354
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-08-15 16:24:21 +0100

    deconstruct weather array

--- src/components/WeatherTile/WeatherTile.jsx
+++ src/components/WeatherTile/WeatherTile.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import "./WeatherTile.scss";
 
-const WeatherTile = ({ weather }) => {
+const WeatherTile = ({
+  weather,
+  city,
+  temp,
+  icon,
+  description,
+  feelsLike,
+  wind,
+  humidity,
+}) => {
   return (
     <div className="weather-tile">
-      <h1 className="weather-tile__city">{weather.name}</h1>
+      <h1 className="weather-tile__city">{city}</h1>
       <div className="weather-tile__content">
         <img
           className="weather-tile__icon"
-          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-          alt={weather.weather[0].description}
+          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+          alt={description}
         />
-        <h2 className="weather-tile__temp">{Math.floor(weather.main.temp)}℃</h2>
+        <h2 className="weather-tile__temp">{temp}℃</h2>
       </div>
-      <p className="weather-tile__feels">
-        Feels like: {Math.floor(weather.main.feels_like)}℃
-      </p>
-      <p className="weather-tile__wind">Wind: {weather.wind.speed}km/h</p>
-      <p className="weather-tile__humidity">
-        Humidity: {weather.main.humidity}%
-      </p>
+      <p className="weather-tile__feels">Feels like: {feelsLike}℃</p>
+      <p className="weather-tile__wind">Wind: {wind}km/h</p>
+      <p className="weather-tile__humidity">Humidity: {humidity}%</p>
     </div>
   );
 };
--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -18,18 +18,36 @@ const Dashboard = () => {
     });
   const key = "5f472b7acba333cd8a035ea85a0d4d4c";
   const [weather, setWeather] = useState([]);
+  const [city, setCity] = useState("London");
+  const [temp, setTemp] = useState(32);
+  const [icon, setIcon] = useState("01d");
+  const [description, setDescription] = useState("clear");
+  const [feelsLike, setFeelsLike] = useState(29);
+  const [wind, setWind] = useState(12);
+  const [humidity, setHumidity] = useState(52);
 
   const getWeather = async () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
     setWeather(data);
+    setWeatherInfo();
     console.log(data);
   };
 
   useEffect(() => {
     getWeather();
   }, []);
+
+  const setWeatherInfo = () => {
+    setCity(weather.name);
+    setTemp(Math.floor(weather.main.temp));
+    setIcon(weather.weather[0].icon);
+    setDescription(weather.weather[0].description);
+    setFeelsLike(Math.floor(weather.main.feels_like));
+    setWind(Math.floor(weather.wind.speed));
+    setHumidity(weather.main.humidity);
+  };
 
   const setCoords = () => {
     console.log(coords.latitude);
@@ -45,7 +63,16 @@ const Dashboard = () => {
   return (
     <div>
       <Nav setLonLat={setLonLat} />
-      <WeatherTile weather={weather} />
+      <WeatherTile
+        weather={weather}
+        city={city}
+        temp={temp}
+        icon={icon}
+        description={description}
+        feelsLike={feelsLike}
+        wind={wind}
+        humidity={humidity}
+      />
       {!isGeolocationAvailable ? (
         <div>Your browser does not support Geolocation</div>
       ) : !isGeolocationEnabled ? (
